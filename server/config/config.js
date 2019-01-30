ffcd5dbf8a6d5742ac9336557333b70ad4752e12
Javier Rincon <javier.rincon@fusionops.com>
2019-01-30 17:12:56 -0400

    Added new dokku config for db

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ process.env.PORT = process.env.PORT || 5000;
  ******************
  */
 
-process.env.MONGOURI = "mongodb://localhost:27017/softars-dashboard";
+process.env.MONGOURI = "mongodb://dashboard:7f3ecef1ef3aea7d7f9bdf852569ea49@dokku-mongo-dashboard:27017/dashboard";
 
 //mongodb://jesus1234:jesus1234@softars-dashboard-shard-00-00-v9jya.mongodb.net:27017,softars-dashboard-shard-00-01-v9jya.mongodb.net:27017,softars-dashboard-shard-00-02-v9jya.mongodb.net:27017/test?ssl=true&replicaSet=SoftArs-Dashboard-shard-0&authSource=admin&retryWrites=true
 /**
