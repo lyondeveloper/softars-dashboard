4b23bd5034189ebd6fa77341e92a33ae40c650ed
Javier Rincon <javier.rincon@fusionops.com>
2019-01-31 08:43:47 -0400

    Change to mlab table

--- server/config/config.js
+++ server/config/config.js
@@ -20,8 +20,8 @@ process.env.PORT = process.env.PORT || 5000;
  */
 
 process.env.MONGOURI =
-    "mongodb://dashboard:7f3ecef1ef3aea7d7f9bdf852569ea49@dokku-mongo-dashboard:27017/dashboard";
-// process.env.MONGOURI = "mongodb://jjrincon:Jr946864@ds147233.mlab.com:47233/sadashboard";
+    // "mongodb://dashboard:7f3ecef1ef3aea7d7f9bdf852569ea49@dokku-mongo-dashboard:27017/dashboard";
+process.env.MONGOURI = "mongodb://jjrincon:Jr946864@ds147233.mlab.com:47233/sadashboard";
 // process.env.MONGOURI = "mongodb://jesus1234:jesus1234@main-cluster-shard-00-00-v9jya.mongodb.net:27017,main-cluster-shard-00-01-v9jya.mongodb.net:27017,main-cluster-shard-00-02-v9jya.mongodb.net:27017/SoftArs-Dashboard?ssl=true&replicaSet=Main-Cluster-shard-0&authSource=admin&retryWrites=true";
 
 /**
