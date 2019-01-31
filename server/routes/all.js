3cf8d9f10180a1f70eeb16de68eb05d69d0272dc
Javier Rincon <javier.rincon@fusionops.com>
2019-01-31 11:54:06 -0400

    Removed another path

--- server/routes/all.js
+++ server/routes/all.js
@@ -10,7 +10,7 @@ const path = require("path");
 router.get(
   "*",
     (req, res, next) => {
-        res.sendFile(path.join(__dirname+'/public/index.html'));
+        res.sendFile(path.resolve('public', 'index.html'));
     }
 );
 
